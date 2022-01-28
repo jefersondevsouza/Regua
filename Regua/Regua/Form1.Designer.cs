adce4c73f17a8b71697b29bc1e444980dd32c510
Jeferson Silveira <jefersoncomp@gmail.com>
2022-01-28 00:16:50 -0300

    Storage checkin
    Checkin inicial para armazenar o código

---
+++ Regua/Regua/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Regua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::Regua.Properties.Resources.regua_virtual;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(840, 140);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(820, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 21);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "x";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button2.Location = new System.Drawing.Point(840, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(10, 140);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 140);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Regua/Regua/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Regua
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.InitMove();
+            this.InitResizeHorizontal();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #region Tamanho da regua
+        bool arrastar = false;
+        int tamanho = 0;
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (arrastar)
+            {
+                this.Size = new Size((this.Size.Width + (e.Location.X - tamanho)), this.Size.Height);
+                tamanho = e.Location.X;
+                Application.DoEvents();
+            }
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+           // arrastar = true;
+            tamanho = e.Location.X;
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            arrastar = false;
+        } 
+        #endregion
+
+        #region Move
+
+        private Point firstPoint = new Point();
+        public void InitMove()
+        {
+            this.MoverControle(this);
+            this.MoverControle(this.pictureBox1);
+        }
+
+        public void MoverControle(Control control)
+        {
+            control.MouseEnter += (ss, ee) =>
+            {
+                this.Cursor = Cursors.NoMove2D;
+            };
+
+            control.MouseLeave += (ss, ee) =>
+            {
+                this.Cursor = Cursors.Default;
+            };
+
+            control.MouseDown += (ss, ee) =>
+            {
+                if (ee.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    firstPoint = Control.MousePosition;
+                }
+            };
+
+            control.MouseMove += (ss, ee) =>
+            {
+                if (ee.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    //criar um ponto temporario
+                    Point temp = Control.MousePosition;
+                    Point res = new Point(firstPoint.X - temp.X, firstPoint.Y - temp.Y);
+                    //aplica o valor ao objeto
+                    this.Location = new Point(this.Location.X - res.X, this.Location.Y - res.Y);
+
+                    //update firstPoint
+                    firstPoint = temp;
+                }
+            };
+        }
+        #endregion
+
+        int vertRigthInic = 0;
+        private void InitResizeHorizontal()
+        {
+            button2.MouseEnter += (ss, ee) =>
+            {
+                this.Cursor = Cursors.SizeWE;
+            };
+
+            button2.MouseLeave += (ss, ee) =>
+            {
+                this.Cursor = Cursors.Default;
+            };
+
+            button2.MouseDown += (ss, ee) =>
+            {
+                if (ee.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    vertRigthInic = Control.MousePosition.X;
+                }
+            };
+
+            button2.MouseMove += (ss, ee) =>
+            {
+                if (ee.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    //criar um ponto temporario
+                    int temp = Control.MousePosition.X;
+                    int res = vertRigthInic - temp;
+                    //aplica o valor ao objeto
+                    this.Size = new Size(this.Width - res, this.Height);
+                    //update firstPoint
+                    vertRigthInic = temp;
+                }
+            };
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
